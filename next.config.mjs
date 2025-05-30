f99ada4f7a7de776577b205b6612c6f5fffa8cf1
code-crafter-480 <justsineup4321@gmail.com>
2025-05-30 18:34:27 +0530

    remove remotePatterns and add domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        // domains: ["randomuser.me"], // ✅ Add allowed external image domains
-        remotePatterns: [
-            {
-              protocol: "https",
-              hostname: "randomuser.me", // Allow images from all domains
-              pathname: "/api/portraits/**", // ✅ required for matching the image URL path
-            },
-          ],
+        domains: ["randomuser.me"], // ✅ Add allowed external image domains
+        // remotePatterns: [
+        //     {
+        //       protocol: "https",
+        //       hostname: "randomuser.me", // Allow images from all domains
+        //       pathname: "/api/portraits/**", // ✅ required for matching the image URL path
+        //     },
+        //   ],
     },
 
     experimental: {              // ✅ Done this before Setup the 'AI-Receipt-scanner' 
