4274129cc26facd7538497113f5a31b4fd625c22
code-crafter-480 <justsineup4321@gmail.com>
2025-05-30 20:48:41 +0530

    Add Loading effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
-import { LayoutDashboard, PenBox } from 'lucide-react'
 import { checkUser } from '@/lib/checkUser'
+import { AddTransactionButton} from './AddTransactionButton'
+import {AddDashboardButton} from './AddDashboardButton'
 
 const Header = async() => {
 
@@ -28,19 +29,14 @@ const Header = async() => {
 
             <div className='flex items-center space-x-4'>
                 <SignedIn>
-                    <Link href="/dashboard" className='text-gray-600 hover:text-blue-600 flex items-center gap-2' >
+                    {/* <Link href="/dashboard" className='text-gray-600 hover:text-blue-600 flex items-center gap-2' >
                         <Button variant="outline">
                             <LayoutDashboard size={18} />
                             <span className='hidden md:inline'>Dashboard</span>
                         </Button>
-                    </Link>
-
-                    <Link href="/transaction/create"> 
-                        <Button className='flex items-center gap-2 hover:opacity-70'>
-                            <PenBox size={18} /> 
-                            <span className='hidden md:inline'>Add Transaction</span>
-                        </Button> 
-                    </Link>
+                    </Link> */}
+                    <AddDashboardButton />
+                    <AddTransactionButton />
                 </SignedIn>
                 <SignedOut>
                     <SignInButton forceRedirectUrl='/dashboard'>
